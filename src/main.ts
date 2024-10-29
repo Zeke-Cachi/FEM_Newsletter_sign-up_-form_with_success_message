9f22a63537cc9794e079a168b544fc6652b028d9
Cachi <ezequielcachi@gmail.com>
2024-10-29 11:43:46 -0300

    started with error msgs

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+//FUNCTIONALITY FOR ERORR STATES
+const input = document.querySelector("input");
+const errorMsg = document.getElementById("errorMsg");
+
+input?.addEventListener("blur", (e) => {});
