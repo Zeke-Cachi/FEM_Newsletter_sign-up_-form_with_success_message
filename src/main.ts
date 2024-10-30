424378f6f6972bce3efe06e7da8fac8c8fffff8b
Cachi <ezequielcachi@gmail.com>
2024-10-30 16:07:14 -0300

    added error msg config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-//FUNCTIONALITY FOR ERORR STATES
-const input = document.querySelector("input");
-const errorMsg = document.getElementById("errorMsg");
+const fadedTomato = "hsla(4, 100%, 67%, 0.1)";
+const tomato = "hsl(4, 100%, 67%)";
 
-input?.addEventListener("blur", (e) => {});
+//FUNCTIONALITY FOR ERORR STATES
+const input: HTMLInputElement | null = document.querySelector("input");
+const errorMsg: HTMLElement | null = document.getElementById("errorMsg");
+const submitButton: HTMLButtonElement | null = document.querySelector("button");
+
+submitButton?.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (input!.value === "" || input!.validity.typeMismatch) {
+    errorMsg!.classList.remove("hidden");
+    input!.style.background = fadedTomato;
+    input!.style.border = `1px solid ${tomato}`;
+    input!.style.color = tomato;
+  } else {
+    input!.removeAttribute("style");
+  }
+});
+
+input?.addEventListener("focus", () => {
+  errorMsg!.classList.add("hidden");
+});
