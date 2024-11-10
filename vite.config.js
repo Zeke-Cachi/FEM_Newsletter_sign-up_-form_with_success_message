717262c17ae5a64a38f04f5a73269b9552414e8a
Cachi <ezequielcachi@gmail.com>
2024-11-10 10:14:42 -0300

    another change to the config

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/FEM_Newsletter_sign-up_-form_with_success_message/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `@import "./src/styles/tailwind.css";`, // Update this path if necessary
-      },
-    },
-  },
 });
